ebe981f118d84fc523e09a4d77cc97863ec01dff
prool <proolix@gmail.com>
2015-05-29 13:55:09 +0300

    fixed bug with delete tmp file

--- unical1.c
+++ unical1.c
@@ -1097,6 +1097,9 @@ if(j==1) fprintf(fid,"%sF%d, S%d\n", groupnames[m],p,p);         }   }  }  }
 fprintf(fid, "\n");                                                            }
 /*==============================================================================*/
 void exitif(int expression,char* message, char* arg)                           {
+#ifdef PROOL
+if (remove(TMPNAME)) {printf("exitif(): can't delete tmp file `%s'\n",TMPNAME); exit(1);}
+#endif
 if(expression==0) return;
 if(message!=0) fprintf(stdout, "%s", message);
 if(arg!=0) fprintf(stdout, "%s", arg);
